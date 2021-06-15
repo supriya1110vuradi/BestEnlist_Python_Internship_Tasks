fb60424fb82f7b6ca6d2fb563b1af487f27d2c72
Supriya Vuradi <60430505+supriya1110vuradi@users.noreply.github.com>
2021-06-15 17:39:36 +0530

    Add files via upload

---
+++ Day4task.py
@@ -0,0 +1,32 @@
+#1)Create three variables (a,b,c) to same value of any integer & do the following
+#a) Divide a by 10
+#b) Multiply b by 50
+#c) Add c value by 60
+
+a=b=c=90
+a=a/10
+b=b*50
+c=c+60
+print(a,b,c)
+
+
+#2)Create a String variable of 5 characters and replace the 3rd character with G
+
+variable="Python"
+print(variable.replace("t", "G"))
+
+#3)Create two values (a,b) of int,float data type & convert the vise versa, Hint : convert a from int to float datatype & b from float to int datatype
+
+a=45
+b=90.51
+print(float(a))
+print(int(b))
+
+
+#Output:
+#1)9.0 4500 150
+#2)PyGhon
+#3)45.0 & 90
+
+
+
